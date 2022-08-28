155661f188438439f5966147da06c54d7dc22f62
Olga Lutskevich <iwenolga@gmail.com>
2022-08-28 20:17:05 +0300

    part 5 finished

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,27 +4,34 @@ import Seo from '../components/seo';
 import { graphql } from 'gatsby'
 
 export const query = graphql`
-  query {
-    allFile {
+query  {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
       }
     }
   }
+  
 `
 
 const BlogPage = ({data}) => {
     return(
         <Layout pageTitle="My Blog Page">
-            <ul>
+          
             {
-                data.allFile.nodes.map(node => (
-                    <li key={node.name}>
-                        {node.name}
-                    </li>
+                data.allMdx.nodes.map(node => (
+                    <article key={node.id}>
+                        <h2>{node.frontmatter.title}</h2>
+                        <p>Posted: {node.frontmatter.date}</p>
+                        <p>{node.excerpt}</p>
+                    </article>
                 )) 
             }
-            </ul>
         </Layout>
     )
 }
