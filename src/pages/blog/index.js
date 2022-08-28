262e1e54bcf420f56ddf05cb43ed08ab5988ac9a
Olga Lutskevich <iwenolga@gmail.com>
2022-08-28 20:51:27 +0300

    part 6 finished

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import Layout from '../components/layout';
-import Seo from '../components/seo';
-import { graphql } from 'gatsby'
+import Layout from '../../components/layout';
+import Seo from '../../components/seo';
+import { Link, graphql } from 'gatsby'
 
 export const query = graphql`
 query  {
@@ -10,9 +10,9 @@ query  {
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          slug
         }
         id
-        excerpt
       }
     }
   }
@@ -26,9 +26,12 @@ const BlogPage = ({data}) => {
             {
                 data.allMdx.nodes.map(node => (
                     <article key={node.id}>
-                        <h2>{node.frontmatter.title}</h2>
-                        <p>Posted: {node.frontmatter.date}</p>
-                        <p>{node.excerpt}</p>
+                      <h2>
+                        <Link to={`/blog/${node.frontmatter.slug}`}>
+                          {node.frontmatter.title}
+                        </Link>
+                      </h2>
+                      <p>Posted: {node.frontmatter.date}</p>
                     </article>
                 )) 
             }
